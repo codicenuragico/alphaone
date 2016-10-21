b12b85809f0f85e1d2bb0f24263eb02622e0f984
codicenuragico <codicenuragico@gmail.com>
2016-10-21 19:30:33 +0200

    autorizzazioni e associazioni per user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020070538) do
+ActiveRecord::Schema.define(version: 20161021160835) do
+
+  create_table "contents", force: :cascade do |t|
+    t.string   "titolo"
+    t.text     "descrizione"
+    t.decimal  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
